60c91438813213d314bd068f8c407390332b9a3b
Jason Lynch <jason@calindora.com>
2010-10-02 10:21:20 -0500

    Fix the human player to allow two human players.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- src/tripletriad.cc
+++ src/tripletriad.cc
@@ -49,19 +49,18 @@ TripleTriad::TripleTriad() :
 	cards[0] = new Card(6, 10, 4, 9, ELEMENT_NONE); // 29
 	cards[1] = new Card(4, 2, 10, 10, ELEMENT_NONE); // 27
 	cards[2] = new Card(8, 10, 6, 5, ELEMENT_NONE); // 29
-	cards[3] = new Card(9, 10, 2, 6, ELEMENT_NONE); // 25
-	cards[4] = new Card(6, 6, 10, 7, ELEMENT_NONE); // 25
-
-	cards[5] = new Card(9, 2, 8, 6, ELEMENT_NONE); // 25
-	cards[6] = new Card(5, 7, 4, 5, ELEMENT_NONE); // 19
-	cards[7] = new Card(5, 2, 5, 3, ELEMENT_NONE); // 11
-	cards[8] = new Card(8, 8, 2, 2, ELEMENT_NONE); // 11
-	cards[9] = new Card(7, 4, 4, 4, ELEMENT_NONE); // 10
+	cards[3] = new Card(2, 9, 10, 6, ELEMENT_NONE); // 25
+	cards[4] = new Card(9, 10, 2, 6, ELEMENT_NONE); // 25
+	
+	cards[5] = new Card(4, 7, 4, 6, ELEMENT_NONE); // 25
+	cards[6] = new Card(5, 1, 3, 1, ELEMENT_NONE); // 19
+	cards[7] = new Card(2, 1, 5, 3, ELEMENT_NONE); // 11
+	cards[8] = new Card(6, 6, 3, 2, ELEMENT_NONE); // 11
+	cards[9] = new Card(5, 3, 3, 6, ELEMENT_NONE); // 10
 	
 	
 	// Configure the game board and its rules.
-	// GameBoard(start_piece, blue_cards, red_cards, same, plus, same wall, elemental)
-	this->_gameBoard = new GameBoard(false, false, false, false, PIECE_BLUE, cards);
+	this->_gameBoard = new GameBoard(false, true, false, false, PIECE_RED, cards);
 }
 
 TripleTriad::~TripleTriad()
@@ -82,13 +81,14 @@ void TripleTriad::run()
 	Player *firstPlayer = new Player(std::shared_ptr<GameBoard>(this->_gameBoard), PIECE_BLUE, PIECE_RED);
 	Player *secondPlayer = new Player(std::shared_ptr<GameBoard>(this->_gameBoard), PIECE_RED, PIECE_BLUE);
 
-	bool human = false;
+	bool blue_human = false;
+	bool red_human = true;
 
 	while (!this->_gameBoard->get_valid_moves().empty())
 	{
 		this->_gameBoard->render(this->_surface);
 		SDL_Flip(this->_surface);
-		if (this->_gameBoard->get_current_piece() == PIECE_BLUE)
+		if (!blue_human && this->_gameBoard->get_current_piece() == PIECE_BLUE)
 		{
 			unsigned int start = SDL_GetTicks();
 			const Move * move = firstPlayer->get_move();
@@ -96,7 +96,7 @@ void TripleTriad::run()
 
 			std::cout << "Time taken: " << ((SDL_GetTicks() - start) / 1000.0) << "s" << std::endl;
 		}
-		else if (!human)
+		else if (!red_human && this->_gameBoard->get_current_piece() == PIECE_RED)
 		{
 			unsigned int start = SDL_GetTicks();
 			const Move * move = secondPlayer->get_move();
@@ -121,11 +121,11 @@ void TripleTriad::run()
 
 	// Loop until the user exits.
 	this->_gameBoard->render(this->_surface);
-/*	SDL_Flip(this->_surface);
+	SDL_Flip(this->_surface);
 	while (true)
 	{
 		this->checkEvent(true);
-	}*/
+	}
 
 //	delete firstPlayer;
 //	delete secondPlayer;
@@ -159,7 +159,13 @@ bool TripleTriad::checkEvent(bool getHumanCard)
 					{
 						if ((event.button.x >= 10 && event.button.x < 100) || (event.button.x >= 414 && event.button.x < 514))
 						{
-							this->_cardChosen = (event.button.y - 10) / 80;
+							if (event.button.x > 414 && event.button.x < 514)
+								this->_cardChosen = 0;
+							else
+								this->_cardChosen = 5;
+
+							this->_cardChosen += (event.button.y - 10) / 80;
+
 							std::cout << "Player chose card: " << this->_cardChosen << std::endl;
 							getHumanCard = false;
 							getHumanDestination = true;
@@ -172,7 +178,7 @@ bool TripleTriad::checkEvent(bool getHumanCard)
 
 						std::cout << "Player chose: " << row << ", " << col << std::endl;
 
-						const Move * move = this->_gameBoard->get_move(cards[this->_cardChosen + 5], row, col);
+						const Move * move = this->_gameBoard->get_move(cards[this->_cardChosen], row, col);
 
 						if (!move)
 						{
